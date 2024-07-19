84df1b7523d2f428af927b9c77caba6be438745a
samstickkz <samstickkz@gmail.com>
2024-07-19 19:11:14 +0100

    next starts here

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,17 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center ">
+        Discover and share <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">Ai-powered prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Promptopia is a platform for ddiscovering and sharing Ai-powered
+        prompts.iscovering and sharing Ai-powered prompts.
+      </p>
+      {/* feed  */}
+    </section>
+  );
 };
 
 export default Home;
