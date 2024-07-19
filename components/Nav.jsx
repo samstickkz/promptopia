7e8f1afa7e28076e914f0d32b9527cffc2c0ca72
samstickkz <samstickkz@gmail.com>
2024-07-19 20:26:32 +0100

    nav logo

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+"use client";
 
+import Image from "next/image";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import { signIn, SignOut, useSession, getProviders } from "next-auth/react";
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="logo"
+          width={40}
+          height={40}
+          className
+        />
+      </Link>
+    </nav>
+  );
 };
 
 export default Nav;
